924b25e7a4ad78290ac5530ef92e05a1a6cd5832
Barry Rhodes <barry@mymedialab.co.uk>
2015-04-15 09:08:03 +0100

    Why you little...

--- php/app/config/database.php
+++ php/app/config/database.php
@@ -49,7 +49,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'listenings_new',
+			'database'  => 'listenings',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
