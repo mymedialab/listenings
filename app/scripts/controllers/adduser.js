9519c7ff548c081262520fe24acf67103c605d50
Peter Mellett <peter@mymedialab.co.uk>
2015-04-17 16:49:14 +0100

    ensure newUser exists in the correct scope for newuser form

--- app/scripts/controllers/adduser.js
+++ app/scripts/controllers/adduser.js
@@ -8,6 +8,8 @@
  * Controller of the listeningsApp
  */
 angular.module('listeningsApp').controller('AddUserCtrl', function ($scope, AuthService, Session, UserService) {
+    $scope.newUser = {};
+
     $scope.addNew = function(details) {
         UserService.addNew(details).then(function() {
             $scope.newUser = {};
