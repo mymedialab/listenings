e8b0955690d7e9e4dacb1fcf272e0fcccd30b604
Barry Rhodes <barry@mymedialab.co.uk>
2015-05-31 16:40:49 +0100

    Putting question-set changes through the shared model now to fix a bug whereby updates are one-shot then stuck forever.

--- app/scripts/services/question-sets.js
+++ app/scripts/services/question-sets.js
@@ -1,105 +1,64 @@
 /* In the api this is a questionnaire? */
-angular.module('listeningsApp').service('questionSets', function(pouchDB, $q, $http) {
+angular.module('listeningsApp').service('questionSets', function(syncedModel) {
     'use strict';
-    var db = pouchDB('questionnaires');
+    var transformForServer = function(details) {
+        return details;
+    };
+    var transformForLocal = function(details) {
+        details.last_updated = details.updated_at;
+        delete details.updated_at;
+        delete details.created_at;
+        return details;
+    };
+
+    var model = syncedModel.create('questionnaires', '/api/questionnaires', 'questionnaires', transformForServer, transformForLocal);
     var self = {};
-    var lastSync;
 
-    var download = function() {
-        return $http.get('/api/questionnaires').then(function(res) {
-            var docs = [];
-            return db.allDocs({}).then(function(existing) {
-                res.data.forEach(function(row) {
-                    row._id = row.name;
-                    existing.rows.some(function(found) {
-                        if (row._id === found.id) {
-                            row._rev = found.value.rev;
-                            return true;
-                        }
-                    });
-                    docs.push(row);
-                });
-
-                return db.bulkDocs(docs).then(function() {
-                    lastSync = new Date().getTime();
-                    return db.allDocs({include_docs: true}); // jshint ignore:line
-                });
-            });
-        }).catch(function() {
-            // @todo uh, if there's an error just pretend it didn't happen?
-            return db.allDocs({include_docs: true}); // jshint ignore:line
-        });
-    };
-
-    self.listSets = function() {
-        // one fetch per 30 seconds
-        if (lastSync && (lastSync > (new Date().getTime() - 30000))) {
-            return db.allDocs({include_docs: true}); // jshint ignore:line
-        }
-        return download();
-    };
-
-    self.forceSync = function() {
-        return download();
-    };
-
-    self.getQuestions = function (setName) {
-        return db.get(setName);
-    };
-
-    self.create = function (details) {
-        var reformattedTaggables = [];
-
-        details.questions = details.questions || [];
-
-        (details.taggable || []).forEach(function(taggableName) {
-            reformattedTaggables.push({name: taggableName, existing: []});
-        });
-
-        details.taggable = reformattedTaggables;
-        details.id       = 'pending';
-        details._id      = details.name;
-
-        return db.put(details).then(function() {
-            return db.get(details._id);
-        }).then(function(doc) {
-            $http.post('/api/questionnaires', details).success(function(data) {
-                doc.id = data.id;
-                db.put(doc);
-            });
-
-            return db.get(details._id);
-        });
-    };
-
-    self.update = function(details) {
-        var reformattedTaggables = [];
-
-        details.questions = details.questions || [];
-
-        (details.taggable || []).forEach(function(taggable) {
-            if (!taggable.existing) {
-                taggable.existing = [];
+    function tranformTaggables(fromView) {
+        return (fromView || []).map(function(taggable) {
+            if (typeof(taggable) === 'string') {
+                return {name: taggable, existing: []};
+            } else {
+                return {
+                    name: taggable.name,
+                    existing: (taggable.existing || [])
+                };
             }
-
-            reformattedTaggables.push(taggable);
-        });
-
-        details.taggable = reformattedTaggables;
-        details.id       = details.id || 'pending';
-        details._id      = details.name;
-
-        return db.put(details).then(function() {
-            return db.get(details._id);
-        }).then(function(doc) {
-            $http.post('/api/questionnaires', details).success(function(data) {
-                doc.id = data.id;
-                db.put(doc);
-            });
-
-            return db.get(details._id);
         });
     };
+
+    /**
+     * Handles either update or create.
+     * @param  {object} details [description]
+     */
+    function store (details) {
+        details.questions = details.questions || [];
+        details.taggable = tranformTaggables(details.taggable);
+
+        return model.store(details);
+    };
+
+    /**
+     * @deprecated Use getAll instead
+     */
+    self.listSets = model.getAll;
+    /**
+     * @deprecated Use get instead
+     */
+    self.getQuestions = model.get;
+    /**
+     * @deprecated Use store instead
+     */
+    self.create = store;
+    /**
+     * @deprecated Use store instead
+     */
+    self.update = store;
+
+    self.store = store;
+    self.get = model.get;
+    self.forceSync = model.forceSync;
+    self.getAll = model.getAll;
 
     return self;
 });
